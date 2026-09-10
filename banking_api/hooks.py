6f1d88b1b3a46affa24747adf9d494bc50cc78f8
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-09-10 17:02:56 +0530

    disable finacle sync cron in hooks.py

--- banking_api/hooks.py
+++ banking_api/hooks.py
@@ -196,9 +196,9 @@ scheduler_events = {
     "cron": {
 
         # sync employees to finacle every night at 2 AM
-        "0 2 * * *": [
-            "banking_api.finacle_sync.sync_employees_to_finacle"
-        ],
+        # "0 2 * * *": [
+        #     "banking_api.finacle_sync.sync_employees_to_finacle"
+        # ],
 
         # run share application sync and payment
         "0 11,14,17 * * 1-6": [
