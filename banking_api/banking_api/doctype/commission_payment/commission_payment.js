d4973ae8e50a00630288232a73b8aeb1b678c4e6
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-09-10 15:36:34 +0530

    Add Pay Now button in all individual record in Commission Payment

--- banking_api/banking_api/doctype/commission_payment/commission_payment.js
+++ banking_api/banking_api/doctype/commission_payment/commission_payment.js
@@ -1,8 +1,57 @@
-// Copyright (c) 2026, Talib Sheikh and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Commission Payment", {
+    refresh(frm) {
+        if (frm.is_new()) return;
+        if (frm.doc.docstatus === 2) return;
+        if (frm.doc.payment_status === "Paid") return;
 
-// frappe.ui.form.on("Commission Payment", {
-// 	refresh(frm) {
+        frm.add_custom_button(__("Pay Now"), () => {
+            if (!frm.doc.agent_operative_account) {
+                frappe.msgprint(__("Agent Operative Account is required."));
+                return;
+            }
 
-// 	},
-// });
+            if (!frm.doc.final_netpay || flt(frm.doc.final_netpay) <= 0) {
+                frappe.msgprint(__("Final Net Pay must be greater than zero."));
+                return;
+            }
+
+            const amount = format_currency(frm.doc.final_netpay);
+            const account = frm.doc.agent_operative_account;
+
+            frappe.confirm(
+                __(
+                    "Initiate commission payment of {0} to operative account {1}?",
+                    [amount, account]
+                ),
+                () => {
+                    frappe.call({
+                        method: "banking_api.banking_api.doctype.commission_payment.commission_payment.pay_now_commission_payment",
+                        args: {
+                            payment_name: frm.doc.name
+                        },
+                        freeze: true,
+                        freeze_message: __("Initiating Finacle commission payment..."),
+                        callback(r) {
+                            const data = r.message || {};
+
+                            frappe.msgprint({
+                                title: __("Commission Payment Result"),
+                                message: data.message || __("Process completed."),
+                                indicator:
+                                    data.status === "success"
+                                        ? "green"
+                                        : (
+                                            data.status === "warning"
+                                                ? "orange"
+                                                : "red"
+                                        )
+                            });
+
+                            frm.reload_doc();
+                        }
+                    });
+                }
+            );
+        }).addClass("btn-primary");
+    }
+});
